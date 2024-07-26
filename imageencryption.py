ddd3761dd83a30e3ecf87e87eb128cd5a6ea9df4
Bhupendra63k <narayanmahesh249@gmail.com>
2024-07-26 18:05:09 +0530

    imageencryption.py

--- imageencryption.py
+++ imageencryption.py
@@ -1,12 +1,11 @@
-
 with open("C:/Users/naray/Downloads/WhatsApp Image 2024-02-20 at 21.15.56.jpeg", "rb") as file:
     image = file.read()
 
 image = bytearray(image)
 key = 20
 
-for v, j in enumerate(image):
-    image[v] = j ^ key
+for a,b in enumerate(image):
+    image[a] = b ^ key
 
 with open("encrypted.jpg", "wb") as file:
     file.write(image)
